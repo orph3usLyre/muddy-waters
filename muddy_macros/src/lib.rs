4ee0f6a7e1a38fd6abd943916b83e01159dc3629
orph3uslyre <orph3uslyre@protonmail.com>
2025-01-21 13:55:25 +0100

    clippy fixes

--- muddy_macros/src/lib.rs
+++ muddy_macros/src/lib.rs
@@ -38,14 +38,12 @@ use meta::{KeyMode, NonObfuscatedText, NonObfuscatedTexts};
 /// Kept separately to embed in the target binary
 pub(crate) static KEY: Lazy<Key> = Lazy::new(|| {
     use const_hex::FromHex;
-    if let Some(muddy_key) = std::env::var_os("MUDDY_KEY") {
-        let Ok(bytes) = <[u8; 32]>::from_hex(muddy_key.as_encoded_bytes()) else {
-            panic!("Can't get bytes from 'MUDDY_KEY' env variable, secret key needs to be a hex string with 64 symbols length.");
-        };
-        Key::clone_from_slice(&bytes)
-    } else {
-        ChaCha20Poly1305::generate_key(&mut OsRng)
-    }
+    std::env::var_os("MUDDY_KEY").map_or_else(|| ChaCha20Poly1305::generate_key(&mut OsRng), |muddy_key| {
+         let Ok(bytes) = <[u8; 32]>::from_hex(muddy_key.as_encoded_bytes()) else {
+             panic!("Can't get bytes from 'MUDDY_KEY' env variable, secret key needs to be a hex string with 64 symbols length.");
+         };
+         Key::clone_from_slice(&bytes)
+     })
 });
 
 /// Used to generate text encryptions at build time
