2044f83b4659c94990feff659e4d089b520efc27
orph7yre <orph3uslyre@protonmail.com>
2025-01-26 17:14:31 +0100

    rework examples

--- muddy/examples/simple.rs
+++ muddy/examples/simple.rs
@@ -1,11 +1,13 @@
-/// Compile this example and grep the binary for 'obfuscated':
-/// $ strings ./target/debug/examples/simple | grep obfuscated
-///
-use muddy::{m, muddy_init};
+use muddy::muddy;
 
-muddy_init!();
-
+// build with `cargo b --example simple`
+// then search the binary for embedded strings:
+// `strings target/debug/examples/simple | grep supersecret`
+//
+// only the non-obfuscated text will show up
 fn main() {
-    println!("{}", m!("My highly obfuscated text"));
-    println!("My non obfuscated static str - ripgrep me");
+    let non_obfuscated = "notsupersecret9001";
+    let obfuscated = muddy!("supersecret42");
+    println!("{}", non_obfuscated);
+    println!("{}", obfuscated);
 }
